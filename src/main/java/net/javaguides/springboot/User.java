02ef9e8b751b878c257a66c4c3c30f9683e237b3
Anandhu <anandhureji33@gmail.com>
2022-08-30 16:33:23 +0530

    models all features finished

--- src/main/java/net/javaguides/springboot/User.java
+++ src/main/java/net/javaguides/springboot/User.java
@@ -2,10 +2,15 @@ package net.javaguides.springboot;
 
 import java.util.Collection;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -29,6 +34,12 @@ public class User {
 	private String lastName;
 	private String email;
 	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+	@JoinTable(name="user_roles",
+	joinColumns = @JoinColumn(
+			name="user_id",referencedColumnName = "id"),
+	inverseJoinColumns = @JoinColumn(name="role_id",referencedColumnName = "id"))
 	private Collection<Role> roles;
 
 }
