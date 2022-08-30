83bb145a7ae43e64c36dd4777af71b69ca7863c4
Anandhu <anandhureji33@gmail.com>
2022-08-30 15:58:23 +0530

    initial commit

---
+++ src/main/java/net/javaguides/springboot/SpringbootRegestrationApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRegestrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRegestrationApplication.class, args);
+	}
+
+}
